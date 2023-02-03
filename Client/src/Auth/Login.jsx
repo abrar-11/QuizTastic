b14068e600841172d8a6127625e907546ee39a50
abrar-11 <abrarahmed2185@gmail.com>
2023-02-03 17:52:43 +0500

    Formik Integration

--- Client/src/Auth/Login.jsx
+++ Client/src/Auth/Login.jsx
@@ -1,28 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { useFormik } from "formik";
+import { toast, Toaster } from "react-hot-toast";
 
 const Login = (props) => {
     // const { type } = useParams();
     const [icon, setIcon] = useState(null);
     const nav = useNavigate();
-  
-
-
-
-    const handleSubmit = (link) => {
-        console.log(link);
-        props.setUser(type);
-        nav("/");
+    const initialValues = {
+        email: "",
+        password: "",
     };
+    console.log("hello woelsd");
+
+    const formik = useFormik({
+        initialValues: initialValues,
+        validate: (values) => {
+            const errors = {};
+            if (!values.email) {
+                errors.email = toast.error("Email address required");
+            }
+
+            return errors;
+        },
+        validateOnBlur: false,
+        validateOnChange: false,
+
+        onSubmit: async (values) => {
+            console.log(values);
+        },
+    });
+
     return (
         <div>
+            <Toaster></Toaster>
             <div className="h-screen md:flex">
                 <div className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-[#091E61] to-[#091e61ee] i justify-around items-center hidden">
                     <div>
                         <h1 className="text-white font-bold text-6xl font-sans ">
-                           QuizTastic
+                            QuizTastic
                         </h1>
-                       
                     </div>
                     <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
                     <div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
@@ -32,23 +49,27 @@ const Login = (props) => {
                 <div className="flex md:w-1/2 justify-center  items-center bg-white">
                     <div className="min-h-screen flex flex-col justify-center relative overflow-hidden w-full">
                         <div className="relative bg-white px-6 shadow-xl rounded-lg ring-1 ring-gray-900/5 sm:max-w-xl lg:w-80 sm:mx-auto py-4">
-                
                             <div className=" my-5">
                                 <div className="py-4 text-base leading-7  text-gray-600 flex flex-col items-center mx-8">
-                                    <form className="w-full ">
+                                    <form
+                                        className="w-full "
+                                        onSubmit={formik.handleSubmit}
+                                    >
                                         <div className="relative z-0 mb-6 w-full group">
                                             <input
                                                 type="email"
                                                 name="login"
                                                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-800 focus:border-indigo-800 appearance-none focus:outline-none focus:ring-0 peer"
                                                 placeholder=" "
-                                                required
+                                                {...formik.getFieldProps(
+                                                    "email"
+                                                )}
                                             />
                                             <label
                                                 for="login"
                                                 className="absolute uppercase text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 text-gray-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                                             >
-                                                Username or Email
+                                                Email
                                             </label>
                                         </div>
                                         <div className="relative z-0 mb-6 w-full group">
@@ -57,7 +78,9 @@ const Login = (props) => {
                                                 name="password"
                                                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-800 focus:border-indigo-800 appearance-none focus:outline-none focus:ring-0 peer"
                                                 placeholder=" "
-                                                required
+                                                {...formik.getFieldProps(
+                                                    "password"
+                                                )}
                                             />
                                             <label
                                                 for="password"
@@ -66,32 +89,38 @@ const Login = (props) => {
                                                 Password
                                             </label>
                                         </div>
+
+                                        <div className="flex  justify-center items-center text-sm mx-8 text-gray-800  sm:space-y-0">
+                                            <span className="cursor-pointer hover:text-indigo-700">
+                                                Forgot Password?
+                                            </span>
+                                        </div>
+                                        <div className="flex flex-row justify-center items-center text-sm text-gray-800 my-8  sm:space-y-0">
+                                            <button
+                                                className="cursor-pointer bg-gray-800 sm:px-8 px-4 py-3 text-gray-50 uppercase"
+                                                type="submit"
+                                            >
+                                                Log In
+                                            </button>
+                                        </div>
                                     </form>
-                                </div>
-                            </div>
-                            <div className="flex  justify-center items-center text-sm mx-8 text-gray-800  sm:space-y-0">
-                                <span className="cursor-pointer hover:text-indigo-700">
-                                    Forgot Password?
-                                </span>
-                            </div>
-                            <div className="flex flex-row justify-center items-center text-sm text-gray-800 my-8  sm:space-y-0">
-                                <div
-                                    className="cursor-pointer bg-gray-800 sm:px-8 px-4 py-3 text-gray-50 uppercase"
-                                    onClick={() => handleSubmit(type)}
-                                >
-                                    Log In
                                 </div>
                             </div>
 
                             <div className=" mb-5 justify-center items-center text-sm  space-x-2 text-gray-800  sm:space-y-0 text-center">
-                               <span className=""> Don't have an account?</span>
-                                <Link to="/register" className="cursor-pointer text-gray-700 font-semibold hover:text-indigo-700">
+                                <span className="">
+                                    {" "}
+                                    Don't have an account?
+                                </span>
+                                <Link
+                                    to="/register"
+                                    className="cursor-pointer text-gray-700 font-semibold hover:text-indigo-700"
+                                >
                                     Register Now
                                 </Link>
                             </div>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
--- Client/src/components/QuizCardSnap.jsx
+++ Client/src/components/QuizCardSnap.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const QuizCardSnap = (props) => {
+    const nav = useNavigate();
+    const handleClick = () => {
+        nav("/quiz");
+    };
     return (
-        <div className="w-60 cursor-pointer  h-52 rounded-md overflow-hidden  shadow-md hover:shadow-lg transition ease-out  hover:bg-blue-50 bg-white">
+        <div
+            className="w-60 cursor-pointer  h-52 rounded-md overflow-hidden  shadow-md hover:shadow-lg transition ease-out  hover:bg-blue-50 bg-white"
+            onClick={() => handleClick()}
+        >
             <div className="flex w-full h-3 overflow-hidden ">
                 <div className="flex-1 h-3 bg-blue-200"></div>
                 <div className="flex-1 h-3 bg-blue-400"></div>
@@ -11,7 +18,9 @@ const QuizCardSnap = (props) => {
 
             <div className=" flex h-full w-full items-center justify-center flex-col space-y-3">
                 {/* <h1 className="text-3xl font-bold">{props.value}</h1> */}
-                <h1 className="text-xl font-bold text-center text-gray-800">Computer Programming</h1>
+                <h1 className="text-xl font-bold text-center text-gray-800">
+                    Computer Programming
+                </h1>
                 <p className="text-xs font-semibold uppercase">30 questions</p>
             </div>
         </div>
